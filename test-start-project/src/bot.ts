06fb8af8b0fcd099b84c49971852d7a7f7beff36
azumag <tsubasa.azumagakito@gmail.com>
2025-06-16 08:23:21 +0900

    chore: remove test files from repository

--- test-start-project/src/bot.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S deno run --allow-all
-// Simple test bot for dependency installation testing
-console.log("Test bot starting...");
-console.log("Dependencies test passed!");
-Deno.exit(0);
